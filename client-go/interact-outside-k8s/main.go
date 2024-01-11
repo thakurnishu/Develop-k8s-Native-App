841923831a304927b0a595ea8c8cf3722ef57f0c
thakurnishu <thakurnishant5102003@gmail.com>
2024-01-11 18:13:00 +0530

    Added sample application to interact outside k8s

---
+++ client-go/interact-outside-k8s/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func KubeconfigHome() string {
+	// To get kubeconfig file location
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Printf("ERROR: \n%v\n", err)
+	}
+	kubeconfigPath := filepath.Join(homeDir, ".kube", "config")
+
+	return kubeconfigPath
+}
+
+func GetClientSet(kubeconfig *string) *kubernetes.Clientset {
+
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	clientSet, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return clientSet
+}
+
+func main() {
+
+	kubeconfigHome := KubeconfigHome()
+	// File location from user ( --kubeconfig ) or homeDir/.kube/config
+	kubeconfig := flag.String("kubeconfig", kubeconfigHome, "Location of KubeConfig file")
+	// NameSpace from user (--namespace) or default
+	nameSpace := flag.String("namespace", "default", "List of pod in NameSpace")
+
+	// Parse flags once
+	flag.Parse()
+
+	clientset := GetClientSet(kubeconfig)
+	ctx := context.Background()
+
+	podList, err := clientset.CoreV1().Pods(*nameSpace).List(ctx, metaV1.ListOptions{})
+	if err != nil {
+		fmt.Printf("ERROR in listing Pod: \n%v\n", err)
+	}
+	fmt.Printf("Pod in %v namespace\n", *nameSpace)
+	for _, pod := range podList.Items {
+		fmt.Println(pod.Name)
+	}
+
+	fmt.Println()
+
+	deploymentList, err := clientset.AppsV1().Deployments(*nameSpace).List(ctx, metaV1.ListOptions{})
+	if err != nil {
+		fmt.Printf("ERROR in listing Deployments: \n%v\n", err)
+	}
+	fmt.Printf("Deployments in %v namespace\n", *nameSpace)
+	for _, deployment := range deploymentList.Items {
+		fmt.Println(deployment.Name)
+	}
+}
